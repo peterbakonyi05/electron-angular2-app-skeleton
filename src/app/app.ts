180a06e2c2506aae8112fd630daaacb1a6936c82
peba02 <peter.bakonyi@betssongroup.com>
2016-06-21 10:16:38 +0200

    feat(routing): improve design

--- src/app/app.ts
+++ src/app/app.ts
@@ -30,6 +30,11 @@ const routes = [
 		provideRouter(routes)
 	],
 	styles: [`
+		md-toolbar button {
+			margin: 0 8px 0 0;
+			min-width: 0;
+			padding: 0 8px;
+		}
 		md-sidenav-layout {
 			bottom: 0;
 			left: 0;
@@ -38,6 +43,16 @@ const routes = [
 			top: 0;
 		}
 		
+		md-sidenav button {
+			float: right;
+			margin: 4px 4px 0 0;
+			min-width: 0;
+			padding: 0 4px;
+		}
+		
+		nav {
+			margin-top: 24px;
+		}
 		nav a {
 			display: block;
 		}
