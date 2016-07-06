4c9135266efb0cdd279b9f919537856ef167193a
peba02 <peter.bakonyi@betssongroup.com>
2016-07-06 09:06:02 +0200

    fix(material): add material input after cleaning up forms

--- src/app/books/components/book-search/book-search.component.ts
+++ src/app/books/components/book-search/book-search.component.ts
@@ -3,14 +3,16 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/distinctUntilChanged';
 import { Component, Output, ChangeDetectionStrategy } from '@angular/core';
 import { FormControl, REACTIVE_FORM_DIRECTIVES } from '@angular/forms';
+import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
 
 @Component({
 	selector: 'ea-book-search',
 	directives: [
+		MD_INPUT_DIRECTIVES,
 		REACTIVE_FORM_DIRECTIVES
 	],
 	template: `
-		<input placeholder="Book title, author" [formControl]="searchControl" type="search">
+		<md-input placeholder="Book title, author" [formControl]="searchControl" type="search"></md-input>
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
